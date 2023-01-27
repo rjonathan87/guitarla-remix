f989d7cbf76f5f350aacd3f9652f381a03275262
Jonathan Mendoza <rjonathan87@gmail.com>
2023-01-27 17:30:48 -0600

    Inicio Carrito

---
+++ app/routes/carrito.jsx
@@ -0,0 +1,20 @@
+import styles from '~/styles/carrito.css'
+
+
+
+const Carrito = () => {
+  return (
+    <main className="contenedor">
+      <h1 className="heading">Carrito de Compras</h1>
+      <div className="contenido">
+
+      </div>
+      <aside className="resumen">
+        <h3>Resumen del Pedido</h3>
+        <p>Total a pagar: $</p>
+      </aside>
+    </main>
+  )
+}
+
+export default Carrito
